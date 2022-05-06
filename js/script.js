d337e107a589e32216bb40e1dabb9312e0976c08
Sumaya Ali <sumayaali@Sumayas-iMac.local>
2022-05-06 07:58:49 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let $buttons = $(".remove");
+
+$("button").on("click", function () {
+    $(".table").append("<h3>" + $("input").val() + "</h3>");
+});
+
+$(".table").on("click", "h3", function() {
+    $(this).closest("h3").fadeOut()
+
+});
 
